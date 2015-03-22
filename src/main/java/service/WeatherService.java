b3075d9734a6859c684ab16d791a34bf52e0a389
Julia Seroshtan <babenko.julia@gmail.com>
2015-03-22 11:44:26 +0200

    Json parse method changed in weather service

--- src/main/java/service/WeatherService.java
+++ src/main/java/service/WeatherService.java
@@ -1,6 +1,5 @@
 package service;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import data.City;
 import data.Weather;
@@ -22,17 +21,16 @@ final public class WeatherService {
         String serviceUrl = "http://api.worldweatheronline.com/free/v2/past-weather.ashx?" +
                 "key={key}&q={city},ua&format=json&date={date}";
         String key = "b868de27c36d1354e818a8447a21a";
+
         ObjectNode objectNode = restTemplate.getForObject(serviceUrl, ObjectNode.class, key, city.getName(), date);
-        if (objectNode.get("data").get("error") != null) {
+        if (objectNode.findValue("error") != null) {
             throw new CityNotFoundException(city.getName());
         }
 
-        JsonNode dailySummary = objectNode.get("data").get("weather").get(0).get("hourly").get(0);
-
-        String temperatureC = dailySummary.get("tempC").asText();
-        String humidity = dailySummary.get("humidity").asText();
-        String windSpeed = dailySummary.get("windspeedKmph").asText();
-        String pressure = dailySummary.get("pressure").asText();
+        String temperatureC = objectNode.findValue("tempC").asText();
+        String humidity = objectNode.findValue("humidity").asText();
+        String windSpeed = objectNode.findValue("windspeedKmph").asText();
+        String pressure = objectNode.findValue("pressure").asText();
         return new Weather(temperatureC, humidity, windSpeed, pressure);
     }
 }
